a0249f83bf645422a77e253de15bc144b0b844cd
godcong <jumbycc@163.com>
2019-01-04 00:19:50 +0800

    add mongodb driver

---
+++ database.go
@@ -0,0 +1,23 @@
+package mongodb
+
+import (
+	"context"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/options"
+	"time"
+)
+
+type Database struct {
+	ctx     context.Context
+	Timeout time.Duration
+	*mongo.Database
+}
+
+func (d *Database) Collection(name string, opts ...*options.CollectionOptions) *Collection {
+	collection := d.Database.Collection(name, opts...)
+	return &Collection{
+		ctx:        d.ctx,
+		Timeout:    d.Timeout,
+		Collection: collection,
+	}
+}
